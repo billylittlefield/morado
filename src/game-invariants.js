4887890fe20da71244ac6c1f5272c7cc0f2590fa
Matt Symonds <matt.symonds@thetileapp.com>
2018-09-11 19:31:21 -0700

    update Tile color and function name to getStagingRowSize

--- src/game-invariants.js
+++ src/game-invariants.js
@@ -7,7 +7,7 @@ export const NUM_TILES_OF_COLOR = 20
 
 export const TILES_PER_FACTORY = 4
 
-export const TILE_COLORS = ['BLUE', 'ORANGE', 'RED', 'BLACK', 'WHITE']
+export const TILE_COLORS = ['BLUE', 'YELLOW', 'RED', 'BLACK', 'WHITE']
 
 export const POINTS_FOR_COMPLETING_ROW = 2
 export const POINTS_FOR_COMPLETING_COLUMN = 7
@@ -15,4 +15,4 @@ export const POINTS_FOR_COMPLETING_COLOR = 10
 
 export const DROPPED_TILE_PENALTIES = [1, 1, 2, 2, 2, 3, 3]
 
-export const getNumTilesNeededToCompletePatternLine = patternLineIndex => patternLineIndex + 1
+export const getStagingRowSize = rowIndex => rowIndex + 1
